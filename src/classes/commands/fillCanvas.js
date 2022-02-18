4059c7255f189bef712b9aa03e68f5e0c1ed690d
Léonce Aklin <leonceaklin@icloud.com>
2022-02-18 09:07:49 +0100

    FillCanvas command

---
+++ src/classes/commands/fillCanvas.js
@@ -0,0 +1,37 @@
+import Command from "../constructors/drawingCommand";
+
+export default class FillCanvasCommand extends Command {
+  static params = {};
+
+  constructor(options) {
+    super(options);
+  }
+
+  estimate(main) {
+    return {
+      requiredTime: (1 - this.main.state.speed) * 500,
+    };
+  }
+
+  prepare(main) {}
+
+  async execute(progress, ctx) {
+    return new Promise((resolve) => {
+      if (progress == 1) {
+        ctx.globalAlpha = 1;
+        ctx.fillStyle = this.state.fillStyle;
+        ctx.beginPath();
+        ctx.rect(0, 0, this.main.canvas.width, this.main.canvas.height);
+        ctx.fill();
+        resolve();
+      } else {
+        ctx.globalAlpha = 1;
+        ctx.drawImage(this.main.taskHandler.cacheCanvas, 0, 0);
+        ctx.globalAlpha = 1 - progress;
+        ctx.drawImage(this.main.taskHandler.previousCanvas, 0, 0);
+        ctx.globalAlpha = 1;
+        resolve();
+      }
+    });
+  }
+}
--- src/definitions/commandNames.js
+++ src/definitions/commandNames.js
@@ -11,6 +11,7 @@ export default [
 
   "fillText",
   "strokeText",
+  "fillCanvas",
 
   "penUp",
   "penDown",
