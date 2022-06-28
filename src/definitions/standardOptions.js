0720c4584f0b5a66bce5be90f29dcf67a8006b06
Vasco Schwarze <leandervasco.schwarze@sciota.io>
2022-06-28 23:37:49 +0200

    Fixed turtle svg image not being drawn in Firefox

--- src/definitions/standardOptions.js
+++ src/definitions/standardOptions.js
@@ -15,5 +15,5 @@ export default {
     textBaseline: "alphabetic",
   },
   image:
-    "data:image/svg+xml,%3Csvg viewBox='0 0 10 10' xmlns='http://www.w3.org/2000/svg' xmlns:bx='https://boxy-svg.com'%3E%3Cdefs%3E%3Cbx:grid x='0' y='0' width='50' height='50'%3E%3C/bx:grid%3E%3C/defs%3E%3Cpath d='M 5 0 L 0 10 L 5 8 L 10 10 L 5 0 Z' style=''%3E%3C/path%3E%3C/svg%3E",
+    "data:image/svg+xml,%3Csvg width='50' height='50' viewBox='0 0 10 10' xmlns='http://www.w3.org/2000/svg' xmlns:bx='https://boxy-svg.com'%3E%3Cdefs%3E%3Cbx:grid x='0' y='0' width='50' height='50'/%3E%3C/defs%3E%3Cpath d='M 5 0 L 0 10 L 5 8 L 10 10 L 5 0 Z' style=''/%3E%3C/svg%3E",
 };
